66dd199fc1e57b664fdb63509723fc404f524376
”sovsianykov” <swebproger@gmail.com>
2021-10-20 16:52:58 +0300

    initial project

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import QuestionButton from "./QuestionButton/QuestionButton";
+
+function App() {
+  return (
+    <div>
+       <QuestionButton/>
+    </div>
+  );
+}
+
+export default App;
