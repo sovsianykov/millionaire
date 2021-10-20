1c836ded7ebc87b542ae2f38a18b84be5b686500
sovsianykov <swebproger@gmail.com>
2021-10-20 20:40:53 +0300

    question btn

--- src/components/QuestionButton/QuestionButton.js
+++ src/components/QuestionButton/QuestionButton.js
@@ -3,11 +3,15 @@ import styles from "./QuestionButton.module.scss";
 
 const QuestionButton = () => {
     return (
-        <div className={styles.main}>
-             <div className={styles.up}/>
+      <div className={styles.questionBtn}>
+          <div className={styles.line}/>
+          <div className={styles.main}>
+              <div className={styles.up}/>
               <h3 className={styles.content}>Some Content</h3>
-             <div className={styles.down}/>
-        </div>
+              <div className={styles.down}/>
+          </div>
+          <div className={styles.line_right}/>
+      </div>
     );
 };
 
