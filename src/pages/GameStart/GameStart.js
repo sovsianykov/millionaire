d26b7998c00e45974af6a2917c46dd82a6ca8cec
”sovsianykov” <swebproger@gmail.com>
2021-10-21 18:16:00 +0300

    startGame2

--- src/pages/GameStart/GameStart.js
+++ src/pages/GameStart/GameStart.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from "./GameStart.module.scss";
+import handImg from "../../assets/hand/hand1.png"
 
 const GameStart = () => {
     return (
         <main className={styles.container}>
-            <section className={styles.leftSide}></section>
-            <section className={styles.rightSide}></section>
+            <section className={styles.leftSide}>
+                <div className={styles.handImgWrapper}>
+                    <img src={handImg} alt="hand" className={styles.handImg} />
+                </div>
+            </section>
+            <section className={styles.rightSide}>
+            </section>
             <section className={styles.inner}>
             </section>
         </main>
