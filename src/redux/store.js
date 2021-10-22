0dc931cc8650a00c4c0d54d36a40c24c6430122e
”sovsianykov” <swebproger@gmail.com>
2021-10-23 00:45:55 +0300

    Make it better done 47

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,14 @@
-import {createStore, combineReducers} from "redux"
-
+import {createStore, applyMiddleware, combineReducers, compose} from "redux"
+import thunk from "redux-thunk";
 import answerReducer from "./answerReduser";
 
 
 export  const rootReducer = combineReducers({
-    answerReducer,
+    answerReducer: answerReducer
 
 })
 
-const store = createStore(rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(rootReducer, compose( applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 
 export default store
