366cd17441cb780f33cd8edccee3f5842ea01b41
”sovsianykov” <swebproger@gmail.com>
2021-10-23 15:41:00 +0300

    Revert "Make it better done 49"
    This reverts commit cd84a1b3e79db83b4c069c5725a00cd7b8117be4.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,4 @@
 import {createStore, applyMiddleware, combineReducers, compose} from "redux"
-
 import thunk from "redux-thunk";
 import answerReducer from "./answerReduser";
 
@@ -9,7 +8,7 @@ export  const rootReducer = combineReducers({
 
 })
 
-const store = createStore(rootReducer, compose( applyMiddleware(thunk)
-   ))
+const store = createStore(rootReducer, compose( applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 
 export default store
