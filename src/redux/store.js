0d7a5813c9f0cdd67203281b89abe43c99605a5b
”sovsianykov” <swebproger@gmail.com>
2021-10-24 20:05:57 +0300

    Game  Mobile1

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,7 +8,6 @@ export  const rootReducer = combineReducers({
 
 })
 
-const store = createStore(rootReducer, compose( applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(rootReducer, compose( applyMiddleware(thunk)))
 
 export default store
