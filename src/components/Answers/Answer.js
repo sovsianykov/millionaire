f32955eef7dc34b6fb04bd5ebfd96f055184bcbc
”sovsianykov” <swebproger@gmail.com>
2021-10-25 14:20:15 +0300

    Game  done

--- src/components/Answers/Answer.js
+++ src/components/Answers/Answer.js
@@ -1,15 +1,10 @@
-import React, {memo, useCallback, useMemo, useState} from "react";
+import React, { memo, useCallback, useMemo, useState } from "react";
 import styles from "./Answer.module.scss";
 import cn from "classnames";
 import AnswerContent from "./AnswerContent";
 
-
-
 const Answer = ({ title, letter, onClick, id, correct }) => {
-
-
   const [active, setActive] = useState(false);
-
 
   let classNames = useMemo(
     () =>
@@ -21,17 +16,33 @@ const Answer = ({ title, letter, onClick, id, correct }) => {
     [id, correct, active]
   );
 
+  let lineLeft = useMemo(
+    () =>
+      cn({
+        [styles.line]: true,
+        [styles.line_correct]: id === correct && active,
+      }),
+    [id, correct, active]
+  );
+  let lineRight = useMemo(
+    () =>
+      cn({
+        [styles.line_right]: true,
+        [styles.line_right_correct]: id === correct && active,
+      }),
+    [id, correct, active]
+  );
   const onClickHandler = useCallback(() => {
     onClick(id);
     setActive(true);
-  }, [id, setActive,onClick]);
+  }, [id, setActive, onClick]);
 
   return (
     <div className={styles.question}>
-      <div className={styles.line} />
+      <div className={lineLeft} />
       <div className={classNames} onClick={onClickHandler}>
         <AnswerContent title={title} letter={letter} />
-        <div className={styles.line_right} />
+        <div className={lineRight} />
       </div>
     </div>
   );
